6dcb75c8403a6923f677a6291a93654276913ef0
sohee0913 <thgml34567@gmail.com>
2022-11-01 16:47:15 +0900

    03

--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i,average;
-	int sum;
+	int i;
 	int grade[SIZE];
+	int score[SIZE];
 	
-	sum=0;
-	for(i=0;i<SIZE;i++){
-	printf("학생 성적을 입력하세요:");
-	scanf("%d", &grade[i]);
-	sum+=grade[i];
-}
-	average=sum/SIZE;
-	printf("성적 평균:%d\n",average);
-
+	for(i=0;i<SIZE;i++)
+		grade[i]=rand()%100;
+	
+	for(i=0;i<SIZE;i++)
+		score[i]=grade[i];
+	
+	for(i=0;i<SIZE;i++)
+		printf("grade[%d]=%d, score[%d]=%d\n",i,grade[i],i,score[i]);
 	return 0;
 }
