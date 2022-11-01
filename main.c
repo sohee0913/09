7303dc6fd1db56a500f13f43b3392674d16f07df
sohee0913 <thgml34567@gmail.com>
2022-11-01 17:04:40 +0900

    04

--- main.c
+++ main.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 5
+#define SIZE 4
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void square_array(int a[],int size);
+void print_array(int a[],int size);
 
 int main(int argc, char *argv[]) {
-	int i;
-	int grade[SIZE];
-	int score[SIZE];
+	int list[SIZE]={1,2,3,4};
 	
-	for(i=0;i<SIZE;i++)
-		grade[i]=rand()%100;
+	print_array(list,SIZE);
+	square_array(list,SIZE);
+	print_array(list,SIZE);
 	
-	for(i=0;i<SIZE;i++)
-		score[i]=grade[i];
-	
-	for(i=0;i<SIZE;i++)
-		printf("grade[%d]=%d, score[%d]=%d\n",i,grade[i],i,score[i]);
 	return 0;
+}
+void square_array(int a[], int size){
+	int i;	
+	for(i=0;i<SIZE;i++)
+		a[i]=a[i]*a[i];	
+}
+
+void print_array(int a[], int size){
+	int i;
+	
+	for(i=0;i<SIZE;i++)
+		printf("%3d", a[i]);
+	printf("\n");
 }
